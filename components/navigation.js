c3ef208ca4d477b2ecbfb8393c411bbe0edd1631
Aaron Cunnington <aaron.cunnington@ripjar.com>
2019-03-24 12:38:50 +0000

    Add some icons.

--- components/navigation.js
+++ components/navigation.js
@@ -1,17 +1,20 @@
-import { Fragment } from "react";
 import { withRouter } from "next/router";
 import Link from "next/link";
+import styled from "styled-components";
 
 const pages = [
   {
+    iconClassName: "fas fa-list",
     label: "Rules",
     href: "/rules"
   },
   {
+    iconClassName: "fas fa-user-plus",
     label: "Sign up",
     href: "/signup"
   },
   {
+    iconClassName: "fab fa-discord",
     label: "Discord",
     href: "/discord"
   }
@@ -19,15 +22,39 @@ const pages = [
 
 export default withRouter(({ router }) => {
   return (
-    <nav>
-      {pages.map(({ href, label }, index) => (
-        <Fragment key={href}>
+    <StyledNav>
+      {pages.map(({ iconClassName, href, label }) => (
+        <StyledLinkContainer key={href}>
           <Link href={href}>
-            <a>{label}</a>
+            <a>
+              {iconClassName && <StyledIcon className={iconClassName} />}
+              {label}
+            </a>
           </Link>
-          {index < pages.length - 1 && " | "}
-        </Fragment>
+        </StyledLinkContainer>
       ))}
-    </nav>
+    </StyledNav>
   );
 });
+
+const StyledNav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+`;
+
+const StyledIcon = styled.i`
+  margin-right: 5px;
+`;
+
+const StyledLinkContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
